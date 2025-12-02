9b175fef4b46bf624a803c4280980299d8f9b18e
MOUSTAFA EZZAT <moustafaelzan6@gmail.com>
2025-12-03 00:27:33 +0200

    SecurityUpdate01

---
+++ Shared/Dtos/Auth_Module/UpdateRoleDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Dtos.Auth_Module
+{
+    public class UpdateRoleDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string NewRole { get; set; } = string.Empty ;
+    }
+}
